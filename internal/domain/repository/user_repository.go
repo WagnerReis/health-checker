aaebc3851aa570c0d6a56f1fc8cdd19ae91242db
Wagner Reis <wagnerreis1997@gmail.com>
2026-02-18 06:55:27 -0300

    feat: add in-memory user repository implementation

---
+++ internal/domain/repository/user_repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"context"
+	entities "health-checker/internal/domain/entity"
+
+	"github.com/gofrs/uuid"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *entities.User) error
+	FindByID(ctx context.Context, id uuid.UUID) (*entities.User, error)
+	FindByEmail(ctx context.Context, email string) (*entities.User, error)
+}
